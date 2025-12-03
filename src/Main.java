ac2f521a2b50c01e0527b88dfaed761ef9233932
Luan Carlos <luan.k28@hotmail.com>
2025-12-03 20:32:30 -0300

    Finalizado Bootcamp e Dev

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.upgrade.desafio.dominio.Conteudo;
-import br.com.upgrade.desafio.dominio.Curso;
-import br.com.upgrade.desafio.dominio.Mentoria;
+import br.com.upgrade.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -11,9 +9,33 @@ public class Main {
         Conteudo cursoJavascript = new Curso("Curso Javascript", "Descrição curso javascript", 4);
         Conteudo mentoriaJava = new Mentoria("Mentoria Java", "Descrição mentoria java", LocalDate.now());
 
+        Bootcamp bootcampJava = new Bootcamp("Bootcamp Java Developer", "Descrição Java Developer");
+        bootcampJava.getConteudos().add(cursoJava);
+        bootcampJava.getConteudos().add(cursoJavascript);
+        bootcampJava.getConteudos().add(mentoriaJava);
 
-        System.out.println(cursoJava);
-        System.out.println(cursoJavascript);
-        System.out.println(mentoriaJava);
+        Dev devLuan = new Dev("Luan Carlos");
+        devLuan.inscreverBootcamp(bootcampJava);
+        Dev devIsa = new Dev("Isadora Vale");
+        devIsa.inscreverBootcamp(bootcampJava);
+
+        System.out.printf("Conteúdos Inscritos %s: %s \n", devLuan.getNome(), devLuan.getConteudosInscritos());
+        System.out.printf("Conteúdos Inscritos %s: %s \n", devIsa.getNome(), devIsa.getConteudosInscritos());
+
+        devLuan.progredir();
+        devIsa.progredir();
+
+        System.out.println("------------------------------------");
+
+        System.out.printf("Conteúdos Concluídos %s: %s \n", devLuan.getNome(), devLuan.getConteudosInscritos());
+        System.out.printf("Conteúdos Concluídos %s: %s \n", devIsa.getNome(), devIsa.getConteudosInscritos());
+
+        System.out.println("\n------------------------------------");
+
+        System.out.printf("Conteúdos Inscritos %s: %s \n", devLuan.getNome(), devLuan.getConteudosInscritos());
+        System.out.printf("Conteúdos Inscritos %s: %s \n", devIsa.getNome(), devIsa.getConteudosInscritos());
+
+        System.out.println("XP: " + devLuan.calcularTotalXp());
+        System.out.println("XP: " + devIsa.calcularTotalXp());
     }
 }
